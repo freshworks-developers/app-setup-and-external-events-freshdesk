4a7df15b30d5de887a08d1a2ac05b550cc79e2d7
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-18 11:29:01 +0530

    🐛 FIX: empty state

--- server/server.js
+++ server/server.js
@@ -3,19 +3,6 @@
 const thirdPartyEndpoint = 'https://fcc15baf9a2e24ef7421ffe101394ca7.m.pipedream.net';
 
 /* ~ start ~*/
-exports = {
-  events: [
-    { event: 'onAppInstall', callback: 'onInstallHandler' },
-    { event: 'onAppUninstall', callback: 'onUninstallHandler' }
-  ],
-  onInstallHandler: function (payload) {
-    console.info(' Observed on App Install ', payload);
-    renderData();
-  },
-  onUninstallHandler: function (payload) {
-    console.info('Observed on App Uninstall', payload);
-    renderData();
-  }
-};
+
 
 /** ~end~ */
